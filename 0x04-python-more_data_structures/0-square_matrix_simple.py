dfb87404b6d5790c9de3fb3a904a192b12b20a5e
mwaskagi <gituiku.mwangi@gmail.com>
2023-01-10 21:45:30 -0800

    Matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    a = []
+    for i in matrix:
+        b = []
+        for j in i:
+            b.append(j**2)
+        a.append(b)
+    return a
