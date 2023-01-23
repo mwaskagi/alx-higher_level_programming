4f8e8b69e873448ba0b256a92712e95e19fafdac
mwaskagi <gituiku.mwangi@gmail.com>
2023-01-23 08:29:36 -0800

    Safe Print List

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     y = 0
     try:
         for a in range(0, x):
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[a]), end="")
             y += 1
         print("")
         return x
