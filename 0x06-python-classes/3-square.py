81be23932813cd51db60f7aeddfea97a439aaa78
mwaskagi <gituiku.mwangi@gmail.com>
2023-01-25 23:04:48 -0800

    Calculate square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Class Square that defines as square """
+
+
+class Square:
+    """ Square Class """
+    def __init__(self, size=0):
+        """ Constructor """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Cal Square Area """
+        return(self.__size * seld.__size)
