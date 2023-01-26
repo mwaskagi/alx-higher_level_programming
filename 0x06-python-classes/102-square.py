178931101efc2eb71931b9d2055c2ab4937a653a
mwaskagi <gituiku.mwangi@gmail.com>
2023-01-26 04:40:56 -0800

    Compare

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -19,7 +19,7 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self. value):
+    def size(self, value):
         """ set size """
         if type(value) is not int:
             raise TypeError("size must be an integer")
