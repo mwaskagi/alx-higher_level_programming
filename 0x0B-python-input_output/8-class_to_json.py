c26cfd33bdce9a7504596680625ebcec16156f95
mwaskagi <gituiku.mwangi@gmail.com>
2023-02-07 11:40:45 -0800

    Dictonary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Functin thate returns the dictionary
+description from json object
+"""
+
+
+def class_to_json(obj):
+    """
+    return the dictionary
+    """
+    return obj.__dict__
