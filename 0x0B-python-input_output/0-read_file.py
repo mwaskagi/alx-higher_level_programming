50702d2738f5db269fde3a137eef6ddf192e3b78
mwaskagi <gituiku.mwangi@gmail.com>
2023-02-07 10:26:59 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@ def read_file(filename=""):
     Args:
         filename(str): filename
     """
-    with open(filename, 'r', encoding="utf-8") as a_file:
+    with open(filename, 'r') as a_file:
         print"{}".format(a_file.read()), end="")
