a2e22d7d7ed8998537671076210f0418a33fc528
mwaskagi <gituiku.mwangi@gmail.com>
2023-04-17 09:29:53 -0700

    Starts with

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import MySQLdb
-import sys
+from sys import argv
+
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2])
+    db = MySQLdb.connect(user=argv[1],passwd=argv[2],db=argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id")
     my_states = c.fetchall()
     for state in my_states:
-        if state[1][0] == 'N':
+        if state[1].startswith("N"):
             print(state)
+    c.close()
+    db.clos()
