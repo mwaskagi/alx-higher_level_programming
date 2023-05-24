aff94f423d32b8a84c294432ed385f68e1fd63d3
mwaskagi <gituiku.mwangi@gmail.com>
2023-05-24 04:44:04 -0700

    films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 // A script that prints the number of movies where the character Wedge Antilles is present
 
 const args = process.argv;
-let reqURL = args[2];
+let reqURL = 'https://swapi-api.alx-tools.com/api/films';
 let request = require('request');
 request(reqURL, function (error, response, body) {
   if (error) {
